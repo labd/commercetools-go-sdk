3500c5174670438229fa2cc4b2523d2e861feb81
Jasper Berghoef <jasper.berghoef@gmail.com>
2018-08-10 14:52:31 +0200

    Simplify extension return values

--- commercetools/customize/extension.go
+++ commercetools/customize/extension.go
@@ -19,61 +19,55 @@ type ExtensionUpdateInput struct {
 	Actions commercetools.UpdateActions
 }
 
-func (svc *Service) ExtensionGetByID(id string) (*Extension, error) {
-	var result Extension
-	err := svc.client.Get(fmt.Sprintf("extensions/%s", id), nil, &result)
+func (svc *Service) ExtensionGetByID(id string) (result *Extension, err error) {
+	err = svc.client.Get(fmt.Sprintf("extensions/%s", id), nil, &result)
 	if err != nil {
 		return nil, err
 	}
-	return &result, nil
+	return result, nil
 }
 
 // ExtensionCreate creates a new API extension.
 // Currently, a maximum of 25 extensions can be created per project.
-func (svc *Service) ExtensionCreate(draft *ExtensionDraft) (*Extension, error) {
-	var result Extension
-	err := svc.client.Create("extensions", nil, draft, &result)
+func (svc *Service) ExtensionCreate(draft *ExtensionDraft) (result *Extension, err error) {
+	err = svc.client.Create("extensions", nil, draft, &result)
 	if err != nil {
 		return nil, err
 	}
-	return &result, nil
+	return result, nil
 }
 
-func (svc *Service) ExtensionUpdate(input *ExtensionUpdateInput) (*Extension, error) {
-	var result Extension
-
+func (svc *Service) ExtensionUpdate(input *ExtensionUpdateInput) (result *Extension, err error) {
 	endpoint := fmt.Sprintf("products/%s", input.ID)
-	err := svc.client.Update(endpoint, nil, input.Version, input.Actions, &result)
+	err = svc.client.Update(endpoint, nil, input.Version, input.Actions, &result)
 	if err != nil {
 		return nil, err
 	}
-	return &result, nil
+	return result, nil
 }
 
-func (svc *Service) ExtensionDeleteByID(id string, version int) (*Extension, error) {
-	var result Extension
+func (svc *Service) ExtensionDeleteByID(id string, version int) (result *Extension, err error) {
 	endpoint := fmt.Sprintf("extensions/%s", id)
 	params := url.Values{}
 	params.Set("version", string(version))
-	err := svc.client.Delete(endpoint, params, &result)
+	err = svc.client.Delete(endpoint, params, &result)
 
 	if err != nil {
 		return nil, err
 	}
-	return &result, nil
+	return result, nil
 }
 
-func (svc *Service) ExtensionDeleteByKey(key string, version int) (*Extension, error) {
-	var result Extension
+func (svc *Service) ExtensionDeleteByKey(key string, version int) (result *Extension, err error) {
 	endpoint := fmt.Sprintf("extensions/key=%s", key)
 	params := url.Values{}
 	params.Set("version", string(version))
-	err := svc.client.Delete(endpoint, params, &result)
+	err = svc.client.Delete(endpoint, params, &result)
 
 	if err != nil {
 		return nil, err
 	}
-	return &result, nil
+	return result, nil
 }
 
 func ExtensionDestinationMapping(input ExtensionDestination) ExtensionDestination {
