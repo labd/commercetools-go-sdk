4ac3d8e1b1a62809b5cb921dc47c5f724b061771
Michael van Tellingen <m.vantellingen@labdigital.nl>
2020-07-07 16:01:14 +0200

    Move useragent logic to it's own file
    It's quite a long function to generate the user agent so this feels
    better

--- commercetools/client.go
+++ commercetools/client.go
@@ -4,13 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
-	"runtime"
 	"strconv"
 	"strings"
 
@@ -124,40 +122,6 @@ func New(cfg *Config) *Client {
 		client.logLevel = 1
 	}
 	return client
-}
-
-// GetUserAgent determines the user agent for all HTTP requests.
-func GetUserAgent(cfg *Config) string {
-	baseInfo := "commercetools-go-sdk/1.0.0"
-	systemInfo := fmt.Sprintf("Go/%s (%s; %s)", runtime.Version(), runtime.GOOS, runtime.GOARCH)
-
-	libraryInfo := ""
-	if cfg.LibraryName != "" && cfg.LibraryVersion == "" {
-		libraryInfo = cfg.LibraryName
-	} else if cfg.LibraryName != "" && cfg.LibraryVersion != "" {
-		libraryInfo = fmt.Sprintf("%s/%s", cfg.LibraryName, cfg.LibraryVersion)
-	}
-	contactInfo := ""
-	if cfg.ContactURL != "" && cfg.ContactEmail == "" {
-		contactInfo = fmt.Sprintf("(+%s)", cfg.ContactURL)
-	} else if cfg.ContactURL == "" && cfg.ContactEmail != "" {
-		contactInfo = fmt.Sprintf("(+%s)", cfg.ContactEmail)
-	} else if cfg.ContactURL != "" && cfg.ContactEmail != "" {
-		contactInfo = fmt.Sprintf("(+%s; +%s)", cfg.ContactURL, cfg.ContactEmail)
-	}
-
-	s := []string{
-		baseInfo,
-		systemInfo,
-	}
-	if libraryInfo != "" {
-		s = append(s, libraryInfo)
-	}
-	if contactInfo != "" {
-		s = append(s, contactInfo)
-	}
-
-	return strings.Join(s, " ")
 }
 
 func getConfigValue(value string, envName string) string {
