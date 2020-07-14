65f0b33750b67bc954e9995e0f5d850c54e46e09
Michael van Tellingen <m.vantellingen@labdigital.nl>
2020-07-14 21:57:54 +0200

    Add custom applicaton support (work in progress)

---
+++ commercetools/service_custom_applications.go
@@ -0,0 +1,145 @@
+package commercetools
+
+import (
+	"context"
+)
+
+// LabelLocale describes the structure of a localized label.
+type CustomApplicationLabelLocale struct {
+	Locale string `json:"locale"`
+	Value  string `json:"value"`
+}
+
+type CustomApplicationNavbarMenu struct {
+	Key             string                           `json:"key"`
+	URIPath         string                           `json:"uriPath"`
+	Icon            string                           `json:"icon"`
+	LabelAllLocales []CustomApplicationLabelLocale   `json:"labelAllLocales,omitempty"`
+	Permissions     []string                         `json:"permissions"`
+	Submenu         []CustomApplicationNavbarSubmenu `json:"submenu"`
+}
+
+type CustomApplicationNavbarSubmenu struct {
+	Key             string                         `json:"key"`
+	URIPath         string                         `json:"uriPath"`
+	LabelAllLocales []CustomApplicationLabelLocale `json:"labelAllLocales"`
+	Permissions     []string                       `json:"permissions"`
+}
+
+type CustomApplicationDraft struct {
+	Name        string                      `json:"name"`
+	Description *string                     `json:"description,omitempty"`
+	URL         string                      `json:"url"`
+	NavbarMenu  CustomApplicationNavbarMenu `json:"navbarMenu"`
+	OAuthScopes []string                    `json:"oAuthScopes,omitempty"`
+}
+
+// CustomApplication describes the structure of a Custom Application stored object.
+type CustomApplication struct {
+	ID          string                      `json:"id"`
+	CreatedAt   string                      `json:"createdAt"`
+	UpdatedAt   string                      `json:"updatedAt"`
+	IsActive    bool                        `json:"isActive"`
+	Name        string                      `json:"name"`
+	Description *string                     `json:"description,omitempty"`
+	URL         string                      `json:"url"`
+	NavbarMenu  CustomApplicationNavbarMenu `json:"navbarMenu"`
+}
+
+// ProjectExtension describes the structure of a project extension stored object.
+type ProjectExtension struct {
+	ID           string              `json:"id"`
+	Applications []CustomApplication `json:"applications"`
+}
+
+// GraphQLResponseProjectExtension describes the structure of the query result for fetching a Custom Application.
+type graphQLResponseProjectExtension struct {
+	ProjectExtension *ProjectExtension `json:"projectExtension"`
+}
+
+// GraphQLResponseProjectExtensionCreation describes the structure of the query result for creating a Custom Application.
+type graphQLResponseProjectExtensionCreation struct {
+	CreateProjectExtensionApplication *ProjectExtension `json:"createProjectExtensionApplication"`
+}
+
+// GraphQLResponseProjectExtensionUpdate describes the structure of the query result for updating a Custom Application.
+type graphQLResponseProjectExtensionUpdate struct {
+	UpdateProjectExtensionApplication     *ProjectExtension `json:"updateProjectExtensionApplication"`
+	ActivateProjectExtensionApplication   *ProjectExtension `json:"activateProjectExtensionApplication"`
+	DeactivateProjectExtensionApplication *ProjectExtension `json:"deactivateProjectExtensionApplication"`
+}
+
+// GraphQLResponseProjectExtensionDeletion describes the structure of the query result for deleting a Custom Application.
+type graphQLResponseProjectExtensionDeletion struct {
+	DeleteProjectExtensionApplication *ProjectExtension `json:"deleteProjectExtensionApplication"`
+}
+
+func (client *Client) CustomApplicationCreate(ctx context.Context, draft *CustomApplicationDraft) (*CustomApplication, error) {
+	var result graphQLResponseProjectExtensionCreation
+
+	query := client.NewGraphQLQuery(`
+		mutation CreateCustomApplication($draft: ApplicationExtensionDataInput!) {
+			createProjectExtensionApplication(data: $draft) {
+				id
+				applications {
+					id
+				}
+			}
+		}`)
+
+	query.Bind("draft", draft)
+	err := query.Execute(&result, query.ForMerchantCenter())
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+
+}
+
+func (client *Client) CustomApplicationGetWithID(ctx context.Context, ID string) (*CustomApplication, error) {
+	var result graphQLResponseProjectExtension
+
+	query := client.NewGraphQLQuery(`
+		query FetchCustomApplicationById($applicationId: ID!) {
+			projectExtension {
+				id
+				applications(where: { id: $applicationId }) {
+					id
+					createdAt
+					updatedAt
+					isActive
+					name
+					description
+					url
+					navbarMenu {
+						uriPath
+						icon
+						labelAllLocales {
+							locale
+							value
+						}
+						permissions
+						submenu {
+							uriPath
+							labelAllLocales {
+								locale
+								value
+							}
+							permissions
+						}
+					}
+				}
+			}
+		}
+		`)
+
+	query.Bind("applicationId", ID)
+	err := query.Execute(&result, query.ForMerchantCenter())
+	if err != nil {
+		return nil, err
+	}
+	if len(result.ProjectExtension.Applications) == 1 {
+		return &result.ProjectExtension.Applications[0], nil
+	}
+	return nil, nil
+}
